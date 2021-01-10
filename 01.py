15500aab0905649eb5b49b277cc92f77b5268da3
Cherrlyy <ind07229@naver.com>
2021-01-10 15:51:09 +0900

    for palindrome

---
+++ 01.py
@@ -0,0 +1,16 @@
+
+def isPalindrome(s: list) -> bool:
+    if len(s) <= 1:
+        return True
+    print(s[0] + ", " + s[-1] + " is " , s[0] == s[-1])
+    # print(strs[0] == strs[-1])
+    return (s[0] == s[-1]) & isPalindrome(s[1:-1])
+
+s = input("")
+strs = []
+
+for char in s:
+    if char.isalnum():
+        strs.append(char.lower())
+is_bool = isPalindrome(strs)
+print(is_bool)
