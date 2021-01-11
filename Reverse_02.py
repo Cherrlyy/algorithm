4541d39028da5d49880563da7051962926d0c871
Cherrlyy <ind07229@naver.com>
2021-01-11 18:54:33 +0900

    for reverse string

--- Reverse_02.py
+++ Reverse_02.py
@@ -1,6 +1,6 @@
 def reverseStr(s: list) -> None:
     left, right = 0, len(s) -1
-    while left < right:
+    while left < right: # 단순 숫자 이용해 for문 돌리기보다 유리함
         s[left], s[right] = s[right], s[left]
         left += 1
         right -= 1
